9f18eaa646a694204df7fe55d52ce13e2d25fbbd
Felipe <felipe.ar@pm.me>
2025-04-24 16:35:29 -0300

    fix: Error on AuthCallback page

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,13 +1,8 @@
 // src/app/auth/callback/page.tsx
 import GEPCallback from "@/components/auth/gep-callback";
-import { useSteamRedirect } from "@/hooks/use-steam-redirect";
 import { Suspense } from "react";
 
 export default function AuthCallback() {
-  const { status } = useSteamRedirect();
-
-  if (status === "loading") return <div>Loading...</div>;
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <GEPCallback />
