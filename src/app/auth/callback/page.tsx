01703a720353261348b0f58e52e381075d87b7bc
Felipe <felipe.ar@pm.me>
2025-04-24 16:23:50 -0300

    fix: Remov Suspense from callback

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -2,19 +2,15 @@
 "use client";
 
 import { useSteamRedirect } from "@/hooks/use-steam-redirect";
-import { Suspense } from "react";
 
 export default function AuthCallback() {
   const { status } = useSteamRedirect();
 
-  if (status === "loading")
-    return <Suspense fallback={<div>Loading...</div>} />;
+  if (status === "loading") return <div>Loading...</div>;
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="min-h-screen flex items-center justify-center">
-        <p>Redirecionando de volta para o GSI Client...</p>
-      </div>
-    </Suspense>
+    <div className="min-h-screen flex items-center justify-center">
+      <p>Redirecionando de volta para o GSI Client...</p>
+    </div>
   );
 }
