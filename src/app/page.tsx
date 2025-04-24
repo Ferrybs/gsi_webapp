1711b4f13296702beb466743a071502306f90b03
Felipe <felipe.ar@pm.me>
2025-04-24 16:14:46 -0300

    fix: Suspense for deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
-"use client";
-
-import { signIn } from "next-auth/react";
-import { Button } from "@/components/ui/button";
-import { useSteamRedirect } from "@/hooks/use-steam-redirect";
+import SteamSignIn from "@/components/signIn/steam-signin";
+import { Suspense } from "react";
 
 export default function HomePage() {
-  useSteamRedirect();
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold">Bem-vindo ao GSI WebApp</h1>
-      <Button
-        onClick={() =>
-          signIn("steam", {
-            callbackUrl: "/auth/callback?redirect=http://localhost:4567/auth",
-          })
-        }
-      >
-        Login com Steam
-      </Button>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SteamSignIn />
+    </Suspense>
   );
 }
